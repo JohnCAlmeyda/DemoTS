d133a1119b357b5a7917ea5c8d4be4f124560a2e
John Jaime Chunqui Almeyda <jchunqui@everis.com>
2018-07-08 17:41:16 -0500

    Eliminacion de ts

--- salida.js
+++
@@ -1,7 +0,0 @@
-var myString = "xxd";
-var myNum = 2;
-var myBool = true;
-var myArray = ["John", "Jaime"];
-var cualquiera;
-cualquiera = 2;
-var sape = "xddd";
--- salida.ts
+++
@@ -1,7 +0,0 @@
-var myString: string = "xxd";
-var myNum: number = 2;
-var myBool: boolean=true;
-var myArray: string[] = ["John","Jaime"];
-var cualquiera: any;
-cualquiera = 2;
-var sape: string = "xddd";
