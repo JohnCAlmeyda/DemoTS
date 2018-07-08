b0539d983654e9e30ac861c5b4f866639a7c0c55
John Jaime Chunqui Almeyda <jchunqui@everis.com>
2018-07-08 17:28:47 -0500

    Tipo de variables tsc

--- salida.js
+++ salida.js
@@ -1,1 +1,7 @@
-console.log("Hola mundo");
+var myString = "xxd";
+var myNum = 2;
+var myBool = true;
+var myArray = ["John", "Jaime"];
+var cualquiera;
+cualquiera = 2;
+var sape = "xddd";
--- salida.ts
+++ salida.ts
@@ -1,2 +1,7 @@
-console.log("H1o1la mundo 123")
-console.log("1H1o1la mundo")
+var myString: string = "xxd";
+var myNum: number = 2;
+var myBool: boolean=true;
+var myArray: string[] = ["John","Jaime"];
+var cualquiera: any;
+cualquiera = 2;
+var sape: string = "xddd";
