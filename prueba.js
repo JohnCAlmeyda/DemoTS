4e297768d7464a86d20a11c7f54803516759c420
John Jaime Chunqui Almeyda <jchunqui@everis.com>
2018-07-08 22:46:55 -0500

    Layout responsive v2.1

---
+++ prueba.js
@@ -0,0 +1,8 @@
+var myString = "Hola";
+var myNum = 322;
+var myBool = true;
+var myArray = ["John", "Pedro", "Javier", "Marco"];
+var myAny = "Cualquier valor";
+var myArray2 = ["John", ["Teléfono", 976324692], false, 1];
+var myTuple = ["John", 20];
+alert(myTuple[0]);
---
+++ prueba.ts
@@ -0,0 +1,8 @@
+var myString: string ="Hola";
+var myNum: number = 322;
+var myBool: boolean =true;
+var myArray: string[]=["John","Pedro","Javier","Marco"];
+var myAny: any= "Cualquier valor";
+var myArray2: any[]=["John",["Teléfono",976324692],false,1];
+var myTuple: [string, number] = ["John",20];
+alert(myTuple[0]);
